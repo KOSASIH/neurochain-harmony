b5de19ae366e888e6d9c0658cf417e148cac709c
KOSASIH <kosasihg88@gmail.com>
2024-08-18 19:36:16 +0700

    Create quantum_resistant_cryptography.py

---
+++ blockchain/consensus/quantum_resistant_cryptography.py
@@ -0,0 +1,68 @@
+import hashlib
+import os
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+
+class QuantumResistantCryptography:
+    def __init__(self, private_key=None, public_key=None):
+        self.private_key = private_key
+        self.public_key = public_key
+        if not private_key and not public_key:
+            self.generate_key_pair()
+
+    def generate_key_pair(self):
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+        )
+        self.public_key = self.private_key.public_key()
+
+    def get_public_key_pem(self):
+        return self.public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+    def get_private_key_pem(self):
+        return self.private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+
+    def encrypt(self, data):
+        cipher = self.public_key.encrypt(
+            data,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return cipher
+
+    def decrypt(self, encrypted_data):
+        plaintext = self.private_key.decrypt(
+            encrypted_data,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return plaintext
+
+    def derive_key(self, salt, info):
+        hkdf = HKDF(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            info=info,
+        )
+        key = hkdf.derive(self.private_key.exchange(self.public_key))
+        return key
+
+    def __str__(self):
+        return f"Quantum Resistant Cryptography (Private Key: {self.private_key
